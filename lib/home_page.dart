5148e4450e785e9fdefeded7781c8471b3ce8e40
Azmarul Islam <imjohnny.dev@gmail.com>
2023-12-27 01:13:05 +0600

    Real-Time Location Tracker

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -15,7 +15,7 @@ class _HomePageState extends State<HomePage> {
   final Location _location = Location();
   final List<LatLng> _polylineCoordinates = [];
   final Set<Polyline> _polylines = {};
-  Marker? _marker;
+  final Set<Marker> _markers = {};
   LatLng? _previousLatLng;
 
   @override
@@ -27,9 +27,9 @@ class _HomePageState extends State<HomePage> {
   void _initLocationService() async {
     _location.onLocationChanged.listen((LocationData locationData) {
       _currentLocation = locationData;
-        _mapController!.animateCamera(CameraUpdate.newLatLng(
-          LatLng(locationData.latitude!, locationData.longitude!),
-        ));
+      _mapController!.animateCamera(CameraUpdate.newLatLng(
+        LatLng(locationData.latitude!, locationData.longitude!),
+      ));
       _updateMapLocation();
     });
   }
@@ -39,23 +39,24 @@ class _HomePageState extends State<HomePage> {
       final newLatLng =
           LatLng(_currentLocation!.latitude!, _currentLocation!.longitude!);
 
-      if (_marker != null && _previousLatLng != null) {
+      if (_previousLatLng != null) {
         _polylines.add(Polyline(
-          polylineId: const PolylineId("polyline"),
+          polylineId: const PolylineId("polylines"),
           color: Colors.blue,
+          width: 3,
           points: [..._polylineCoordinates, newLatLng],
         ));
       }
       _polylineCoordinates.add(newLatLng);
 
-      _marker = Marker(
-        markerId: const MarkerId("marker"),
+      _markers.add(Marker(
+        markerId: const MarkerId("markers"),
         position: newLatLng,
         infoWindow: InfoWindow(
           title: "My Current Location",
           snippet: "Lat: ${newLatLng.latitude} , Lng: ${newLatLng.longitude}",
         ),
-      );
+      ));
 
       _previousLatLng = newLatLng;
       _mapController!.animateCamera(CameraUpdate.newLatLng(newLatLng));
@@ -75,7 +76,7 @@ class _HomePageState extends State<HomePage> {
         onMapCreated: (controller) {
           _mapController = controller;
         },
-        markers: _marker != null ? <Marker>{_marker!} : <Marker>{},
+        markers: _markers,
         polylines: _polylines,
         myLocationEnabled: true,
         myLocationButtonEnabled: true,
