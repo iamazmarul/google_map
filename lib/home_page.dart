1a7a95296385c2bebfc03d93424cac7ca5d61a98
Azmarul Islam <imjohnny.dev@gmail.com>
2023-12-27 01:04:53 +0600

    Real-Time Location Tracker

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,56 +10,75 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  GoogleMapController? _mapController;
+  LocationData? _currentLocation;
+  final Location _location = Location();
+  final List<LatLng> _polylineCoordinates = [];
+  final Set<Polyline> _polylines = {};
+  Marker? _marker;
+  LatLng? _previousLatLng;
+
+  @override
+  void initState() {
+    super.initState();
+    _initLocationService();
+  }
+
+  void _initLocationService() async {
+    _location.onLocationChanged.listen((LocationData locationData) {
+      _currentLocation = locationData;
+        _mapController!.animateCamera(CameraUpdate.newLatLng(
+          LatLng(locationData.latitude!, locationData.longitude!),
+        ));
+      _updateMapLocation();
+    });
+  }
+
+  void _updateMapLocation() {
+    if (_currentLocation != null && _mapController != null) {
+      final newLatLng =
+          LatLng(_currentLocation!.latitude!, _currentLocation!.longitude!);
+
+      if (_marker != null && _previousLatLng != null) {
+        _polylines.add(Polyline(
+          polylineId: const PolylineId("polyline"),
+          color: Colors.blue,
+          points: [..._polylineCoordinates, newLatLng],
+        ));
+      }
+      _polylineCoordinates.add(newLatLng);
+
+      _marker = Marker(
+        markerId: const MarkerId("marker"),
+        position: newLatLng,
+        infoWindow: InfoWindow(
+          title: "My Current Location",
+          snippet: "Lat: ${newLatLng.latitude} , Lng: ${newLatLng.longitude}",
+        ),
+      );
+
+      _previousLatLng = newLatLng;
+      _mapController!.animateCamera(CameraUpdate.newLatLng(newLatLng));
+    }
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Google Map"),
-        backgroundColor: Colors.blueAccent,
+        title: const Text("Real-Time Location Tracker"),
       ),
       body: GoogleMap(
-        initialCameraPosition: const CameraPosition(
-          zoom: 17,
-          bearing: 0,
-          target: LatLng(22.710361, 90.363845),
-        ),
-        zoomGesturesEnabled: false,
-        compassEnabled: false,
+        initialCameraPosition:
+            const CameraPosition(target: LatLng(0, 0), zoom: 15),
+        onMapCreated: (controller) {
+          _mapController = controller;
+        },
+        markers: _marker != null ? <Marker>{_marker!} : <Marker>{},
+        polylines: _polylines,
         myLocationEnabled: true,
         myLocationButtonEnabled: true,
-        markers: <Marker>{
-          Marker(
-            markerId: const MarkerId("initialPosition2"),
-            position: const LatLng(22.710361, 90.363845),
-            icon:
-                BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
-            infoWindow: const InfoWindow(
-                title: "Cicada Solutions", snippet: "IT Company"),
-            draggable: true,
-          ),
-          Marker(
-            markerId: const MarkerId("initialPosition"),
-            position: const LatLng(22.715527, 90.374472),
-            icon: BitmapDescriptor.defaultMarkerWithHue(
-                BitmapDescriptor.hueGreen),
-            infoWindow:
-                const InfoWindow(title: "Pixel Tech", snippet: "IT Company"),
-            draggable: true,
-          ),
-        },
-        polylines: {
-          const Polyline(
-              polylineId: PolylineId("line"),
-              color: Colors.red,
-              width: 3,
-              patterns: [
-                PatternItem.dot,
-              ],
-              points: [
-                LatLng(22.710361, 90.363845),
-                LatLng(22.715527, 90.374472),
-              ])
-        },
       ),
     );
   }
